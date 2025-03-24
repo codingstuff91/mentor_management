21cd6b806b2501b20644cb0b4be6b8428417bb4f
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-03-24 21:26:50 +0100

    Remove redundant duration-related tests from CourseServiceTest

--- tests/Unit/CourseServiceTest.php
+++ tests/Unit/CourseServiceTest.php
@@ -1,33 +1,6 @@
 <?php
 
 use App\Services\CourseService;
-use Carbon\Carbon;
-
-it('Gets the hours count from the duration', function () {
-    $duration = "01:00";
-
-    $expectedHoursCount = CourseService::splitHoursOrMinutes($duration, 0);
-
-    expect($expectedHoursCount)->toBe(1);
-});
-
-it('Gets the minutes count from the duration', function () {
-    $duration = "01:30";
-
-    $expectedHoursCount = CourseService::splitHoursOrMinutes($duration, 1);
-
-    expect($expectedHoursCount)->toBe(30);
-});
-
-it('Computes the end hour of a course based on start hour and duration', function () {
-    $startHourCourse = Carbon::parse('08:00');
-
-    $courseDuration = "01:30";
-
-    $endHourCourse = CourseService::computeEndHour($startHourCourse, $courseDuration);
-
-    expect($endHourCourse)->toBe('09:30');
-});
 
 it('calculates the total price of a course based on duration and hourly rate', function ($duration, $expectedTotalPrice) {
     $hourlyRate = 10;
