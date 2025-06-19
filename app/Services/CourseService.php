b57ab3210653f1adc6ad9ad88af2c57c51490ec6
codingstuff91 <codingstuff91@gmail.com>
2025-06-19 18:36:35 +0200

    Add service methods and unit tests

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -2,14 +2,32 @@
 
 namespace App\Services;
 
+use App\Models\Course;
 use Carbon\Carbon;
 
 class CourseService
 {
     public static function calculate_total_price(string $duration, int $hourlyRate): float
     {
-        $hoursCount = Carbon::parse($duration)->hour + (Carbon::parse($duration)->minute / 60);
+        $durationInHours = self::tranform_course_duration_in_minutes_into_HH_MM_format($duration);
+
+        $hoursCount = Carbon::parse($durationInHours)->hour + (Carbon::parse($durationInHours)->minute / 60);
 
         return $hoursCount * $hourlyRate;
+    }
+
+    public static function transform_duration_in_minutes(string $courseDuration): float
+    {
+        [$hours, $minutes, $seconds] = explode(":", $courseDuration);
+
+        return ($hours * 60 + $minutes);
+    }
+
+    public static function tranform_course_duration_in_minutes_into_HH_MM_format(string $courseDuration): string
+    {
+        $hours = floor($courseDuration / 60);
+        $remainingMinutes = $courseDuration % 60;
+
+        return sprintf('%02d:%02d', $hours, $remainingMinutes);
     }
 }
---
+++ tests/Unit/Services/CourseServiceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Services\CourseService;
+
+it('calculates the total price of a course based on duration and hourly rate', function ($duration, $expectedTotalPrice) {
+    $hourlyRate = 10;
+
+    $courseTotalPrice = CourseService::calculate_total_price($duration, $hourlyRate);
+
+    expect($courseTotalPrice)->toBe($expectedTotalPrice);
+})->with([
+    'One hour' => [60, 10.0],
+    'Two hours' => [120, 20.0],
+    'Two hours and 30 minutes' => [150,25.0],
+]);
+
+
+it('transform course duration into minutes', function () {
+    $courseDuration = "03:00:00";
+
+    $courseDurationInSeconds = CourseService::transform_duration_in_minutes($courseDuration);
+
+    expect($courseDurationInSeconds)->toBe(180.0);
+});
+
+it('transform course duration in minutes into hour format', function () {
+    $courseDuration = 150;
+
+    $courseDurationInSeconds = CourseService::tranform_course_duration_in_minutes_into_HH_MM_format($courseDuration);
+
+    expect($courseDurationInSeconds)->toBe("02:30");
+});
+
+
